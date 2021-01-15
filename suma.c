686e49805b46becc3d9d066d9ffd8812c79bd525
davidpopescu <73894454+davidpopescu@users.noreply.github.com>
2021-01-15 14:15:29 +0200

    update2

--- suma.c
+++ suma.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
  
 void main(int argc, char * argv[]) {
    int i, sum = 0;
  
    if (argc != 3) {
-      printf("Introduceti 2 numere");
+      printf("You have forgot to type numbers.");
       exit(1);
    }
  
-   printf("Suma este ");
+   printf("The sum is : ");
  
    for (i = 1; i < argc; i++)
       sum = sum + atoi(argv[i]);
  
    printf("%d", sum);
  
+}
